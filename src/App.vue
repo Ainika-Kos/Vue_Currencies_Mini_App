07bfe1563c2094fac21ddfdb6a636ef321914725
Ainika <aina.kostina@gmail.com>
2021-01-08 15:26:44 +0200

    Created app without components

--- src/App.vue
+++ src/App.vue
@@ -1,157 +1,90 @@
 <template>
-  <div class="todoApp">
-    <h1 v-if="!todo.length" class="heading">
-      You don't have any task!
-    </h1>
-    <h1 v-else-if="todo.length < 2" class="heading">
-      You have {{ todo.length }} task
-    </h1>
-    <h1 v-else class="heading">
-      You have {{ todo.length }} tasks
-    </h1>
-    <p class="action-text">Add new task:</p>
-    <div class="action-wrapper">
-      <input
-        type="text"
-        v-model="newTask"
-        placeholder="Enter your task"
-        @keyup.enter="addTask(newTask)"
-        class="inputTask"
-      />
-      <Button label="Add task" @onClick="addTask(newTask)" />
-    </div>
-    <p class="action-text">Filter tasks:</p>
-    <div class="action-wrapper">
-      <Button label="All tasks"  @onClick="showAllTasks()" />
-      <Button label="Active tasks"  @onClick="showActiveTasks()" />
-      <Button label="Completed tasks" @onClick="showDoneTasks()" />
-    </div>
-    <div class="task-wrapper">
-      <div v-for="task in filteredList" :key="task.id" class="task">
-        <div>
-          <input type="checkbox" :id="task.id" v-model="task.completed" />
-          <label
-            :for="task.id"
-            class="task__text"
-            :class="{ finished: task.completed }"
-          >
-            {{ task.name }}
-          </label>
-        </div>
-        <Button label="Delete task" @onClick="removeTask(task.id)" />
+  <div class="currenciesApp">
+    <div class="show-currency-wrapper">
+      <div
+        v-for="currency in filteredCurrenciesList"
+        :key="currency.id"
+        className="show-currency"
+      >
+        {{ currency.name }}
+        <Button
+          label="X"
+          class="xButton"
+          @onClick="currency.isActive = !currency.isActive"
+        />
       </div>
     </div>
-    <p v-show="todo.length" class="action-text">Actions with all tasks:</p>
-    <div v-show="todo.length" class="action-wrapper">
-      <Button label="Remove all" @onClick="removeAllTasks()" />
-      <Button label="All done" @onClick="finishAllTasks()" />
-      <Button label="All active" @onClick="unFinishAllTasks()" />
+    <div class="choise-currency-wrapper">
+      <div
+        v-for="currency in currencies"
+        :key="currency.id"
+        class="choise-currency"
+        :class="{ activeChoise: currency.isActive }"
+        @click="currency.isActive = !currency.isActive"
+      >
+        <span>
+          <span
+            class="choise-currency__checkbox"
+            :class="{ red: currency.isActive }"
+          >
+            X
+          </span>
+        </span>
+        <span class="choise-currency__name">{{ currency.name }}</span>
+      </div>
     </div>
   </div>
 </template>
 
 <style src="./app.scss" lang="scss" scoped></style>
-<style src="vue-flexboxgrid/dist/vue-flexboxgrid.css"/>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
-/* import flexboxgrid from 'flexboxgrid'; */
 import Button from './components/button/button.vue';
-/* import Input from './components/input/input.vue'; */
 
-type Data = {
-  header: string;
-  newTask: string;
-  showTask: 'all' | 'active' | 'done';
-};
-
-type Todo = {
+type Currency = {
   id: number;
   name: string;
-  completed: boolean;
+  isActive: boolean;
+};
+
+type Data = {
+  currencies: Currency[];
 };
 
 const Component = defineComponent({
   components: {
     Button,
-    // Input,
   },
-  data() {
+  data(): Data {
     return {
-      header: 'ToDo list',
-      newTask: '',
-      showTask: 'all',
-      todo: [
-        {
-          id: 0,
-          name: 'Buy milk',
-          completed: false,
-        },
-        {
-          id: 1,
-          name: 'Buy bread',
-          completed: false,
-        },
-        {
-          id: 2,
-          name: 'Buy banana',
-          completed: false,
-        },
+      currencies: [
+        { id: 1, name: 'eur', isActive: false },
+        { id: 2, name: 'pln', isActive: false },
+        { id: 3, name: 'gel', isActive: false },
+        { id: 4, name: 'dkk', isActive: false },
+        { id: 5, name: 'czk', isActive: false },
+        { id: 6, name: 'gbp', isActive: false },
+        { id: 7, name: 'sek', isActive: false },
+        { id: 8, name: 'usd', isActive: false },
+        { id: 9, name: 'rub', isActive: false },
       ],
     };
   },
   computed: {
-    filteredList(): Todo[] {
-      let filteredTodo = [...this.todo];
-      if (this.showTask === 'active') {
-        filteredTodo = this.todo.filter((item) => item.completed === false);
-      } else if (this.showTask === 'done') {
-        filteredTodo = this.todo.filter((item) => item.completed === true);
-      }
-      return filteredTodo;
+    filteredCurrenciesList(): Currency[] {
+      const filtresdCurrencies = this.currencies.filter(
+        (item) => item.isActive,
+      );
+
+      return filtresdCurrencies;
     },
   },
   methods: {
-    removeTask(id: number) {
-      const index = this.todo.findIndex((item) => item.id === id);
-      this.todo.splice(index, 1);
-    },
-    finishTask(id: number) {
-      const index = this.todo.findIndex((item) => item.id === id);
-      this.todo[index].completed = !this.todo[index].completed;
-    },
-    addTask(newTask: string) {
-      if (newTask) {
-        const newTodoTask = {
-          id: this.todo.length + 1,
-          name: newTask,
-          completed: false,
-        };
-        this.todo.push(newTodoTask);
-        this.newTask = '';
-      }
-    },
-    showAllTasks() {
-      this.showTask = 'all';
-    },
-    showActiveTasks() {
-      this.showTask = 'active';
-    },
-    showDoneTasks() {
-      this.showTask = 'done';
-    },
-    removeAllTasks() {
-      this.todo = [];
-    },
-    finishAllTasks() {
-      for (let i = 0; i < this.todo.length; i += 1) {
-        this.todo[i].completed = true;
-      }
-    },
-    unFinishAllTasks() {
-      for (let i = 0; i < this.todo.length; i += 1) {
-        this.todo[i].completed = false;
-      }
+    changeCurrencyStatus(id: number) {
+      const index = this.currencies.findIndex((item) => item.id === id);
+
+      this.currencies[index].isActive = !this.currencies[index].isActive;
     },
   },
 });
---
+++ src/components/schowCurrency/showCurrency.vue
@@ -0,0 +1,24 @@
+<template>
+  <div
+    v-for="currency in currencies"
+    :key="currency.id"
+    class="currency-wrapper"
+  >
+    <div className="currency">
+      {{ currency.name }} {{currency.id}}
+      <Button label="X" @onClick="currency.isActive = !currency.isActive" />
+    </div>
+  </div>
+</template>
+
+<style src="./showCurrency.scss" lang="scss" scoped></style>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+const Component = defineComponent({
+  props: ['label', 'currencies'],
+});
+
+export default Component;
+</script>
